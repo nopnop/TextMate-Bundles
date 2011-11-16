ab82095e3f3c62ef06f13b44d643a2acd19439ae
Ben Truyman <bentruyman@gmail.com>
2011-11-16 11:30:06 -0600

    Git protocol, not HTTP

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ TM_DIR=$APP_DIR/TextMate
 BACKUP_DIR=$TM_DIR-Backup
 
 mv "$TM_DIR" "$BACKUP_DIR"
-git clone http://github.com/bentruyman/TextMate-Bundles.git "$TM_DIR"
+git clone git://github.com/bentruyman/TextMate-Bundles.git "$TM_DIR"
 git --git-dir="$TM_DIR"/.git submodule update --init
