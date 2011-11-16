b162504e9fb408f1e4d6355b73c117bda42edc8f
Ben Truyman <bentruyman@gmail.com>
2011-11-16 11:06:04 -0600

    Added install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+APP_DIR    = "~/Library/Application Support/"
+TM_DIR     = $APP_DIR/Textmate
+BACKUP_DIR = $TM_DIR-Backup
+
+mv $TM_DIR $BACKUP_DIR
+git clone git://github.com/bentruyman/TextMate-Bundles.git $TM_DIR
+cd TextMate
+git --git-dir=$TM_DIR submodule update --init
+
+cd $CUR_DIR
